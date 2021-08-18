4c4e196f00da37b5767b29ec6d2fb3d99fb583ba
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-18 18:18:52 +0800

    Updated App component styling

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -7,12 +7,12 @@ const App: React.FC = () => {
         <main className='app'>
             <header className='app__header'>
                 <h1 className='app__title'>Poundit</h1>
-                <h2 className='app__version'>V 0.0.1</h2>
+                <h2 className='app__version'>v0.0.1</h2>
             </header>
 
             <aside className='app__buttons'>
-                <Button handler={() => null}>Buttons Text</Button>
-                <Button handler={() => null}>Buttons Text</Button>
+                <Button handler={() => null}>B</Button>
+                <Button handler={() => null}>B</Button>
             </aside>
 
             <DrumMachine />
