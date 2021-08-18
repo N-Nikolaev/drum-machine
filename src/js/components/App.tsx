9a17c2e74a2e9ed7486f5129851e0e9a0011903c
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-18 15:44:35 +0800

    Added Display component

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Button from './Button'
 
-const App = () => {
+const App: React.FC = () => {
     return (
         <main className='app'>
-            test
+            <header className='app__header'>
+                <h1 className='app__title'>Poundit</h1>
+                <h2 className='app__version'>V 0.0.1</h2>
+            </header>
+
+            <aside className='app__buttons'>
+                <Button handler={() => null}>Buttons Text</Button>
+                <Button handler={() => null}>Buttons Text</Button>
+            </aside>
+
+            <section className="app__content">
+                
+
+            </section>
         </main>
     )
 }
---
+++ src/js/components/Display.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Display
