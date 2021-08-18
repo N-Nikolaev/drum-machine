765967587bf4316bf0387662b4ffcb5f5a9d11a2
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-18 16:55:01 +0800

    Map out Pad component for each 'key'

--- src/js/components/DrumMachine.tsx
+++ src/js/components/DrumMachine.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Display from './Display'
+import Pad from './Pad'
 
 
 const DrumMachine: React.FC = () => {
+    const [keys, setKeys] = useState([
+      'q',
+      'w',
+      'e',
+      'a',
+      's',
+      'd',
+      'z',
+      'x',
+      'c',  
+    ]);
+
     return (
         <article className="drum-machine">
             <Display padName='' />
             <section className='drum-machine__drum-pads'>
-
+                {keys.map((key: string) => {
+                    return <Pad key={key} keyTrigger={key} />
+                })}
             </section>
         </article>
     )
