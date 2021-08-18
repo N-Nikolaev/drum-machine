b42802b328385fda5ca87943d176b18dd67e6689
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-18 15:00:31 +0800

    Changed App component name to Container

---
+++ src/js/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container: React.FC = () => {
+    return (
+        <main className='container'>
+            This is a React TS app
+        </main>
+    )
+}
+
+export default Container
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Container from './components/Container';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
-    <App />,
+    <Container />,
     root
 );
