7123ae0c185aa7f56d3c7a93b2d6de011ae28766
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-28 15:12:40 +0800

    Add drum sound clip  URLs

--- src/js/components/DrumMachine.tsx
+++ src/js/components/DrumMachine.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Display from './Display'
 import Pad from './Pad'
 
-export interface IState {
+interface IState {
     drumPads: {
         key: string,
         url: string,
@@ -10,16 +10,16 @@ export interface IState {
 }
 
 const DrumMachine: React.FC = () => {
-    const [keys, setKeys] = useState<IState['drumPads']>([
-        { key: 'q', url: '' },
-        { key: 'w', url: '' },
-        { key: 'e', url: '' },
-        { key: 'a', url: '' },
-        { key: 's', url: '' },
-        { key: 'd', url: '' },
-        { key: 'z', url: '' },
-        { key: 'x', url: '' }, 
-        { key: 'c', url: '' },
+    const [keys] = useState<IState['drumPads']>([
+        { key: 'q', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+        { key: 'w', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+        { key: 'e', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+        { key: 'a', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+        { key: 's', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+        { key: 'd', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+        { key: 'z', url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+        { key: 'x', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' }, 
+        { key: 'c', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
     ]);
 
     return (
@@ -27,7 +27,12 @@ const DrumMachine: React.FC = () => {
             <Display padName='' />
             <section className='drumMachine__drum-pads'>
                 {keys.map((keyTrigger: {key: string, url: string}) => {
-                    return <Pad key={keyTrigger.key} keyTrigger={keyTrigger.key} url={keyTrigger.url}/>
+                    return (
+                        <Pad 
+                            key={keyTrigger.key} 
+                            keyTrigger={keyTrigger.key} 
+                            url={keyTrigger.url}/>
+                    )
                 })}
             </section>
         </article>
