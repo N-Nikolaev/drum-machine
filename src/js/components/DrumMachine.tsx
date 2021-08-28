5ce69ab02f723d4439adeb37bc8749b68f24fab9
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-28 13:11:57 +0800

    Update DrumMachine component typing

--- src/js/components/DrumMachine.tsx
+++ src/js/components/DrumMachine.tsx
@@ -2,26 +2,32 @@ import React, { useState } from 'react'
 import Display from './Display'
 import Pad from './Pad'
 
+export interface IState {
+    drumPads: {
+        key: string,
+        url: string,
+    }[]
+}
 
 const DrumMachine: React.FC = () => {
-    const [keys, setKeys] = useState([
-      'q',
-      'w',
-      'e',
-      'a',
-      's',
-      'd',
-      'z',
-      'x',
-      'c',  
+    const [keys, setKeys] = useState<IState['drumPads']>([
+        { key: 'q', url: '' },
+        { key: 'w', url: '' },
+        { key: 'e', url: '' },
+        { key: 'a', url: '' },
+        { key: 's', url: '' },
+        { key: 'd', url: '' },
+        { key: 'z', url: '' },
+        { key: 'x', url: '' }, 
+        { key: 'c', url: '' },
     ]);
 
     return (
         <article className="drumMachine">
             <Display padName='' />
             <section className='drumMachine__drum-pads'>
-                {keys.map((key: string) => {
-                    return <Pad key={key} keyTrigger={key} />
+                {keys.map((keyTrigger: {key: string, url: string}) => {
+                    return <Pad key={keyTrigger.key} keyTrigger={keyTrigger.key} url={keyTrigger.url}/>
                 })}
             </section>
         </article>
