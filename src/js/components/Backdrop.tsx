3468d6c1eadd24a26ea0b242bbaa12f8b5b7e7bb
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-09-04 20:18:56 +0800

    Create Backdrop component

---
+++ src/js/components/Backdrop.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface IProps {
+    handler: () => void
+}
+
+const Backdrop: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Backdrop
