e5e3ab10e2fc845b78cab8a15528db6508980178
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-09-05 10:44:04 +0800

    Update Modal component conditional rendering

--- src/js/components/Modal.tsx
+++ src/js/components/Modal.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
+import Button from './Button'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReply } from '@fortawesome/free-solid-svg-icons'
+
 
 interface IProps {
-    children: React.ReactNode;
+    modalOpenHandler: React.Dispatch<React.SetStateAction<boolean>>
+    modalOpen: boolean
+    modalType: string
 }
 
-const Modal: React.FC<IProps> = ({children}) => {
+const Modal: React.FC<IProps> = ({
+    modalOpenHandler,
+    modalOpen,
+    modalType }) => {
+
+    const modalTypeRender = (type: string): JSX.Element  => {
+        switch (type) {
+            case 'theme': 
+                return (
+                    <div>
+                        {modalType} modal
+                    </div>
+                )
+            case 'help': 
+                return (
+                    <div>
+                        {modalType} modal
+                    </div>
+                )
+            default: 
+                return <></>
+        }
+    }
+
     return (
         <aside className='modal'>
-            {children}
+            <Button
+                modalOpen={modalOpen}
+                modalOpenHandler={modalOpenHandler}>
+                    <FontAwesomeIcon icon={faReply} />
+            </Button>
+           {modalTypeRender(modalType)}
         </aside>
     )
 }
